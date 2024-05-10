880ee6b9e7586d45d769d8f56b497f37e8d9878a
I Nyoman Budiarta <105336493+mangbudiarta@users.noreply.github.com>
2024-05-10 16:21:52 +0800

    initial commit: set up project

---
+++ script.js
@@ -0,0 +1,33 @@
+function bukaPesan() {
+  const konten = document.querySelector(".konten");
+  const isiPesan = document.querySelector(".isiPesan");
+  const loader = document.querySelector(".box-loader");
+  loader.style.display = "block";
+  konten.style.display = "none";
+  setTimeout(() => {
+    isiPesan.removeAttribute("hidden");
+    loader.style.display = "none";
+  }, 2500);
+  setTimeout(ketikSurat, 1000);
+}
+
+function balasPesan() {
+  const pesanWhatsapp = "Aku sayang budi💕 ";
+  window.open("https://wa.me/+6283114123271?text=" + pesanWhatsapp, "_blank");
+}
+
+let isiSurat = document.querySelector(".pesan");
+let ucapanSurat =
+  "Halo ayang, aku memang bukan orang yang romantis, tapi selamat ulang tahun sayangg 😘❤️";
+let i = 0;
+let speed = 90;
+isiSurat.value = "";
+function ketikSurat() {
+  if (i < ucapanSurat.length) {
+    isiSurat.value += ucapanSurat.charAt(i);
+    i++;
+    setTimeout(ketikSurat, speed);
+  } else {
+    document.querySelector(".btnWa").removeAttribute("hidden");
+  }
+}
